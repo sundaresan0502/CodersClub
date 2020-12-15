6e1cfc010089a377fc6a281cf14afaed1c4d3e95
Sundaresan <75483601+Sundaresan0502@users.noreply.github.com>
2020-12-15 17:41:53 +0530

    Add files via upload

---
+++ Compiler/c.php
@@ -0,0 +1,400 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+    
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Offline Compiler website</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" type="image/png" href="img/ruet.png">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/jquery-1.12.0.min.js"></script>
+		  <script src="js/vendor/online_i.js"></script>
+
+
+<style>
+*{
+	margin:0px;
+}
+.wrap{
+	width:1024px;             
+	margin:auto;
+	background:white;
+}
+.logo{
+width:1024px;         
+	height:75px;
+	background:green;
+	float:left;
+}
+.header{
+width:1024px;         
+	height:25px;
+	background:black;
+	float:left;
+}
+
+.nav1{
+width:1024px;         
+	height:auto;
+	background:yellow;
+}
+.container{
+	background:orange;
+	height:auto;
+	width:1024px;  
+display: flex;
+  justify-content: left;	
+	
+}
+.nav{
+	width:50%;
+	height:200px;
+	float:left;
+	background:red;
+}
+.rg{
+	width:50%;
+	height:200px;
+	float:right;
+}
+
+.clear{
+	clear:both;    
+}
+.footer{
+	height:50px;
+	background:#ccc;
+	width:1024px;         
+}
+
+ 
+table {
+  width:100%;
+}
+
+table, th,td {
+  border: 1px solid black;
+  
+  }
+th {
+  padding:25px;
+  text-align: top;    
+}
+
+tr{
+	padding:25px;
+		width:100%;
+height:50%;	
+}
+
+td{
+	padding:40x;
+	 text-align:left;
+
+}-->
+
+ a:link {
+color: #b50759;
+}
+a:visited {
+color: #999999;
+}
+a:hover {
+color: #ff3300;
+} 
+
+	
+
+</style>
+
+
+
+
+
+
+</head>
+<body>
+<div class="wrap">
+<div class="logo"> 
+    	<h3 style="text-align:center;">CODERS CLUB CHALLENGING CONTEST</h3>
+  </div> <!-- header tag end-->
+<div class="header">
+
+<h3 style="text-align:left;"><font color="#b50759">CODING PRACTICES FOR THE ACADEMIC YEAR - 2019</font></h3>
+</div ><!-- div header end tag-->
+
+<div class="nav1">
+<p align="justify" style="line-height: 150%"><font face="cam">Let L1 and L2 be 
+two sorted list created by linked list representatin. <br>
+Write procedure mergeLits(L1, L2) to merge L1 and L2 into single list L such <br>
+that L is also sorted.
+</div ><!-- div nav1 end tag-->
+<div class="container"> <!--content start tag-->
+<div  id="nav">
+<table>
+  <tr style="color:white">
+  
+    <th bgcolor="#700000">WEEK</th>
+	<th bgcolor="#700000">Day1</th>
+	<th bgcolor="#700000">Day2</th>
+	<th bgcolor="#700000">Day3</th>
+	<th bgcolor="#700000">Day4</th>
+	<th bgcolor="#700000">Day5</th>
+	   
+  </tr>
+  
+  <tr>
+   <td>WEEK 1 </td>
+    <td><a href="#day1">15-07-19</a></td>
+	<td><a href="#day2">16-07-19</a></td>
+	 <td><a href="#day3">17-07-19</a></td>
+	  <td><a href="#day4">18-07-19</a></td>
+	    <td><a href="#day5">19-07-19</a></td>
+  </tr>
+     
+	 <tr>
+   <td>WEEK 2 </td>
+    <td><a href="#day6">22-07-19</a></td>
+	<td><a href="#day7">23-07-19</a></td>
+	 <td><a href="#day8">24-07-19</a></td>
+	  <td><a href="#day9">25-07-19</a></td>
+	   <td><a href="#day10">26-07-19</a></td>
+  </tr>
+  <tr>
+   <td>WEEK 3 </td>
+    <td><a href="#day11">29-07-19</a></td>
+	<td><a href="#day12">30-07-19</a></td>
+	 <td><a href="#day13">31-07-19</a></td>
+	  <td><a href="#day14">01-08-19</a></td>
+	   <td><a href="#day15">02-08-19</a></td>
+  </tr>
+  
+  
+ 
+</table>
+
+</div><!-- div left end tag-->
+
+<div id="rg"> <!-- right start tag-->
+ 
+<div id="day1" class="toggle" style="display:none">
+<pre class="aLF-aPX-K0-aPE">Circular movement to Matrix cell values
+
+Sample Input: 
+2  4
+6  8
+Sample Output:
+6  2
+8  4
+---
+Sample Input:
+2    4     6
+8   10   12
+14  16  18
+
+Sample Output:
+8     2     4
+14  10    6  
+16  18   12</pre>
+</div>
+
+<div id="day2" class="toggle" style="display:none">
+<img src="img/day2.jpg" width="450px" /></div>
+<div id="day3" class="toggle" style="display:none">
+<img src="img/day3.jpg" width="450px" /></div>
+<div id="day4" class="toggle" style="display:none">
+<img src="img/day4.jpg" width="600px" /></div>
+
+<div id="day5" class="toggle" style="display:none">
+
+<h5>Given two arrays A and B of positive integers of the same size N. The task is to find the maximum sum of products of their elements. Each element in A has to be multiplied with exactly one element in B or vice versa such that each element of both the arrays appears exactly once and the sum of the product obtained is maximum.&nbsp;</h5>
+<h5>Input&nbsp;: A[] = {1, 2, 3}<br />B[] = {4, 5, 1}<br />Output&nbsp;: 24</h5>
+<div>Explanation&nbsp;: Maximum sum of product is obtained by 5*3+4*2+1*1 = 24.</div>
+
+</div>
+
+<div id="day6" class="toggle"  style="display:none">
+<p>Write a procedure getNthItem() such that it takes a linked list and an integer<br /> index and returns the data value stored in the node at that index position. <br /> For ex. If the list contains {42, 13, 666} and the integer value is 1, then <br /> getNthItem() should return 13. Assume the index ranges from [0 .. N-1]. If not, <br /> return error status.</p></p></div>
+<div id="day7" class="toggle" style="display:none">
+<h5><span style="font-family: 'courier new', courier;">Let L1 and L2 be two sorted list created by linked list representatin. <br />Write procedure mergeLits(L1, L2) to merge L1 and L2 into single list L such <br />that L is also sorted.</span></h5></div>
+<div id="day8" class="toggle" style="display:none">
+<p>Write suitable procedure FrontBackSplit(L) to split the list created by using <br />Linked list. For example, let L contain {1,2,3,4,5}, after FrontBackSplit(), L is<br />splitted into L1 = {1,2,3} and L2 = {4,5} based on the length of L.</p></div>
+<!--<div id="day9" class="toggle" style="display:none">
+<p>Write procedure RemoveDuplicates(L) in sorted list to remove duplicate<br />entries from the list created by Linked list. Procedure should use one traversal.</p></div>
+<div id="day10" class="toggle" style="display:none">
+<p>Write proceudre AlternateSplit(L) for Linked List L such that <br />L = {a, b, a, b, a, c, d} get splitted into L1 = {a,a,a,d} and L2 = {b,b,c}</p></div>
+
+<div id="day11" class="toggle" style="display:none">
+<!--<img src="img/day11.jpg" width="450px" /><p></p></div>
+<div id="day12" class="toggle" style="display:none">
+<img src="img/day12.jpg" width="450px" /><p></p></div>
+<div id="day13" class="toggle" style="display:none">
+<img src="img/day13.jpg" width="450px" /><p></p></div>
+<div id="day14" class="toggle" style="display:none">
+<img src="img/day14.jpg" width="450px" /><p></p></div>
+<div id="day15" class="toggle" style="display:none">
+<img src="img/day15.jpg" width="450px" /><p></p></div>-->
+
+  
+  </div> <!-- right end tag-->
+  
+  
+<form action="compile.php" id="form" name="f2" method="POST" >
+<label for="lang">Choose Language</label>
+
+<select class="form-control" name="language">
+<!--<option value="c">C</option>
+<option value="cpp">C++</option>
+<option value="cpp11">C++11</option>
+<option value="java">Java</option>--
+ <option value="python37">Python3</option>-->
+<option value="c">C</option>
+
+</select><br><br>
+
+<label for="ta">Write Your Code</label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label for="ta"></label>
+<textarea class="form-control" name="code" rows="10" cols="50"></textarea><br><br>
+<label for="in">Enter Your Input</label>
+<textarea class="form-control" name="input" rows="10" cols="50"></textarea><br><br>
+<input type="submit" id="st" class="btn btn-success" value="Run Code"><br><br><br>
+
+
+</form>
+
+<script type="text/javascript">
+  
+  $(document).ready(function(){
+
+     $("#st").click(function(){
+  
+           $("#div").html("Loading ......");
+
+
+     });
+
+  });
+
+
+</script>
+
+<script>
+//wait for page load to initialize script
+$(document).ready(function(){
+    //listen for form submission
+    $('form').on('submit', function(e){
+      //prevent form from submitting and leaving page
+      e.preventDefault();
+
+      // AJAX 
+      $.ajax({
+            type: "POST", //type of submit
+            cache: false, //important or else you might get wrong data returned to you
+            url: "compile.php", //destination
+            datatype: "html", //expected data format from process.php
+            data: $('form').serialize(), //target your form's data and serialize for a POST
+            success: function(result) { // data is the var which holds the output of your process.php
+
+                // locate the div with #result and fill it with returned data from process.php
+                $('#div').html(result);
+            }
+        });
+    });
+});
+</script>
+
+<label for="out">Output</label>
+<textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>
+
+<!--<script>
+"use strict";
+function submitForm(oFormElement)
+{
+  var xhr = new XMLHttpRequest();
+  var display=document.getElementById('div');
+  xhr.onload = function(){ display.innerHTML=xhr.responseText; }
+  xhr.open (oFormElement.method, oFormElement.action, true);
+  xhr.send (new FormData (oFormElement));
+  return false;
+}
+</script>-->
+<!--<label for="out">Output</label>
+<textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>-->
+
+
+
+
+
+
+<script>
+
+$("#nav a").click(function(e){
+    e.preventDefault();
+    $(".toggle").hide();
+    var toShow = $(this).attr('href');
+    $(toShow).show();
+});
+</script>
+  
+</div ><!-- div container end tag-->
+
+</div ><!-- div wrap end tag-->
+
+
+<div class="footer">
+<div class="col-sm-3">
+<!-- BEGIN: Powered by Supercounters.com -->
+<!--<center><script type="text/javascript" src="http://widget.supercounters.com/online_i.js"></script><script type="text/javascript">sc_online_i(1360839,"ffffff","e61c1c");</script><br><noscript><a href="http://www.supercounters.com/">Free Online Counter</a></noscript>
+</center>-->
+
+<center>
+ <!-- Start of CuterCounter Code -->
+<a href="https://www.cutercounter.com/" target="_blank"><img src="https://www.cutercounter.com/hits.php?id=gemqkqxx&nd=6&style=59" border="0" alt="hit counter"></a>
+<!-- End of CuterCounter Code -->
+<!--<a href="https://172.17.18.18/compiler/" title= view counter"><img src="https://counter4.wheredoyoucomefrom.ovh/private/freecounterstat.php?c=34dxmppze4lej3fexa6872j3bguxk5xj" border="0" title="free website hit counter"></a>
+
+<!--<a href="https://172.18.18" title= view counter"<img src="https://counter4.wheredoyoucomefrom.ovh/private/freecounterstat.php?c=34dxmppze4lej3fexa6872j3bguxk5xj" border="0" title="website hits counter" ></a>-->
+</center>
+
+<!-- END: Powered by Supercounters.com -->
+
+</div>
+
+<div class="col-sm-5">
+
+
+<div class="fm">
+
+
+<br>
+<b>Developed in IIC, IT Department</b>
+
+</div>
+</div>
+
+
+<div class="col-sm-4">
+<?php
+date_default_timezone_set("Asia/kolkata");
+ $t=date("H:i:s");
+//$currentDate = date('Y-m-d', $currentTimeinSeconds); 
+  // prints the current date 
+ $d = date(DATE_RSS);
+echo"<b>Server Time:  $d</b>";
+
+?>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
---
+++ Compiler/c1.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html>
+<head>
+  
+    
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Offline Compiler website</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" type="image/png" href="img/ruet.png">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/vendor/jquery-1.12.0.min.js"></script>
+		  <script src="js/vendor/online_i.js"></script>
+
+
+
+
+
+
+
+
+</head>
+<body>
+<div class="main">
+
+  <div class="row">
+  <div class="col-sm-12">
+  <nav class="navbar navbar-inverse navbar-fixed-top nbar">
+    <div class="navbar-header">
+      <a class="navbar-brand lspace" href="home.php">Offline Compiler</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="space"><a href="compiler.php"><i class="fa fa-code ispace"></i>Compiler</a></li>
+      <li class="space"><a href=""><i class="fa fa-archive ispace"></i></a></li>
+      <li class="space"><a href=""><i class="fa fa-cogs ispace"></i></a></li>
+    
+      
+    </ul>
+  
+</nav>
+</div>
+</div>
+
+
+<div class="row log">
+<div class="col-sm-10">
+<div class=""><h3 style="text-align:center;">Offline Compiler</h3></div>
+</div>
+
+<div class="col-sm-1">
+
+</div>
+
+<div class="col-sm-1">
+  
+</div>
+
+</div>
+
+
+
+
+<div class="row cspace">
+<div class="col-sm-8">
+<div class="form-group">
+<form action="compile.php" id="form" name="f2" method="POST" >
+<label for="lang">Choose Language</label>
+
+<select class="form-control" name="language">
+<!--<option value="c">C</option>
+<option value="cpp">C++</option>
+<option value="cpp11">C++11</option>
+<option value="java">Java</option>--
+ <option value="python37">Python3</option>-->
+<option value="c">C</option>
+
+</select><br><br>
+
+<label for="ta">Write Your Code</label>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<label for="ta">(IF You select Java Program ,Mention Class Name is Main)</label>
+<textarea class="form-control" name="code" rows="10" cols="50"></textarea><br><br>
+<label for="in">Enter Your Input</label>
+<textarea class="form-control" name="input" rows="10" cols="50"></textarea><br><br>
+<input type="submit" id="st" class="btn btn-success" value="Run Code"><br><br><br>
+
+
+</form>
+
+<script type="text/javascript">
+  
+  $(document).ready(function(){
+
+     $("#st").click(function(){
+  
+           $("#div").html("Loading ......");
+
+
+     });
+
+  });
+
+
+</script>
+
+<script>
+//wait for page load to initialize script
+$(document).ready(function(){
+    //listen for form submission
+    $('form').on('submit', function(e){
+      //prevent form from submitting and leaving page
+      e.preventDefault();
+
+      // AJAX 
+      $.ajax({
+            type: "POST", //type of submit
+            cache: false, //important or else you might get wrong data returned to you
+            url: "compile.php", //destination
+            datatype: "html", //expected data format from process.php
+            data: $('form').serialize(), //target your form's data and serialize for a POST
+            success: function(result) { // data is the var which holds the output of your process.php
+
+                // locate the div with #result and fill it with returned data from process.php
+                $('#div').html(result);
+            }
+        });
+    });
+});
+</script>
+
+<label for="out">Output</label>
+<textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>
+
+<!--<script>
+"use strict";
+function submitForm(oFormElement)
+{
+  var xhr = new XMLHttpRequest();
+  var display=document.getElementById('div');
+  xhr.onload = function(){ display.innerHTML=xhr.responseText; }
+  xhr.open (oFormElement.method, oFormElement.action, true);
+  xhr.send (new FormData (oFormElement));
+  return false;
+}
+</script>-->
+<!--<label for="out">Output</label>
+<textarea id='div' class="form-control" name="output" rows="10" cols="50"></textarea><br><br>-->
+
+
+
+
+</div>
+</div>
+
+<div class="col-sm-4">
+
+  
+</div>
+</div>
+</div>
+<br><br><br>
+<div class="area">
+<div class="well foot">
+<div class="row area">
+<div class="col-sm-3">
+<!-- BEGIN: Powered by Supercounters.com -->
+<center><script type="text/javascript" src="http://widget.supercounters.com/online_i.js"></script><script type="text/javascript">sc_online_i(1360839,"ffffff","e61c1c");</script><br><noscript><a href="http://www.supercounters.com/">Free Online Counter</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+
+</div>
+
+
+<a href="https://www.freecounterstat.com" title="free website hit counter"><img src="https://counter4.wheredoyoucomefrom.ovh/private/freecounterstat.php?c=l8kc5aj912np9r3pye5qyxbbugjukkzw" border="0" title="free website hit counter" alt="free website hit counter"></a>
+
+<div class="col-sm-5">
+
+
+<div class="fm">
+
+
+<br>
+<b>Developed in PITLAB, IT Department</b>
+
+</div>
+</div>
+
+
+<div class="col-sm-4">
+<?php
+date_default_timezone_set("Asia/kolkata");
+ $t=date("H:i:s");
+echo"<b>Server Time:  $t</b>";
+
+?>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
+
+
